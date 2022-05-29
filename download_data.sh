abeca5724a51c50bc13b036feae00ef1e3b0f0ce
Jaka Šircelj <sircelj.jaka@gmail.com>
2022-05-29 10:57:18 +0000

    Add: module2 + homework 2

--- download_data.sh
+++ download_data.sh
@@ -4,6 +4,10 @@
 wget -NP modules/data https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-01.parquet
 wget -NP modules/data https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-02.parquet
 
+wget -NP data https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-01.parquet
+wget -NP data https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-02.parquet
+wget -NP data https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-03.parquet
+
 # Download data for homeworks
 wget -NP homework/data https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-01.parquet
 wget -NP homework/data https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-02.parquet
